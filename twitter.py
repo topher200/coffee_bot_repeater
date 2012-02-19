7ba8d3f53005c09beb8e27a607fa9561d1ae187d
Topher Brown <topher200@gmail.com>
2012-02-19 01:33:50 -0500

    twitter: adding wrapper function to retry failed twitter calls

--- twitter.py
+++ twitter.py
@@ -8,30 +8,53 @@ import urllib2
 SUPERTWEET_USER = os.environ['SUPERTWEET_USER']
 SUPERTWEET_PASS = os.environ['SUPERTWEET_PASS']
 
+def _try_function_multiple_times(func):
+  result = None
+  success = False
+  try_number = 0
+  while (not success) and try_number < 5:
+    try:
+      result = func()
+      success = True
+    except (urllib2.URLError, urllib2.HTTPError), e:
+      logging.info('Dangerous function try #%s failed. error: %s' %
+                   (try_number, e))
+      try_number += 1
+      time.sleep(2**try_number/10)
+  if not success:
+    logging.warning('Dangerous function failed permanently')
+    return False
+  return result
+  
+  
 def get_latest_tweet():
   '''Returns the latest tweet in JSON form.
 
-  In case of urllib2 error, returns None.'''
-  r = urllib2.Request(("https://api.twitter.com/1/statuses/user_timeline.json" \
-                         "?screen_name=fscoffeebot&count=1"))
-  try:
+  In case of a prohibitive number of urllib2 errors, returns False.'''
+  def func():
+    r = urllib2.Request(("https://api.twitter.com/1/statuses/user_timeline.json" \
+                           "?screen_name=fscoffeebot&count=1"))
     tweets = json.load(urllib2.urlopen(r))
-  except urllib2.URLError, e:
-    logging.warn('urllib error: %s', e)
-    return None
-  return tweets[0]
+    return tweets[0]
+  return _try_function_multiple_times(func)
 
 def send_dm(user, message):
+  '''Sends the message as a Twitter DM to the given user.
+
+  Returns True if we receive a good response from the server. Returns False if
+  the response is bad (not 200), or if we are unable to communicate with the
+  server.
+  '''
   logging.info('sending DM to user: %s', user)
-  req = urllib2.Request('http://api.supertweet.net/1/direct_messages/new.json')
-  req.add_data(urllib.urlencode({'user': user, 'text': message}))
-  auth = 'Basic ' + base64.urlsafe_b64encode('%s:%s' % (SUPERTWEET_USER,
-                                                        SUPERTWEET_PASS))
-  req.add_header('Authorization', auth)
-  try:
+  def func():
+    req = urllib2.Request(
+      'http://api.supertweet.net/1/direct_messages/new.json')
+    req.add_data(urllib.urlencode({'user': user, 'text': message}))
+    auth = 'Basic ' + base64.urlsafe_b64encode('%s:%s' % (SUPERTWEET_USER,
+                                                          SUPERTWEET_PASS))
+    req.add_header('Authorization', auth)
     response = urllib2.urlopen(req)
-  except urllib2.HTTPError, e:
-    logging.error('urllib2 error')
-    logging.error(e.read())
-    return False
-  return int(response.getcode()) == 200
+    return int(response.getcode()) == 200
+  return _try_function_multiple_times(func)
+  
+  
